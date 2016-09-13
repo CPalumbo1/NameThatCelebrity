4a2d04177a8fd65e0d617081d557652e4f872682
CPalumbo1 <colton.palumbo@aol.com>
2016-09-12 19:18:26 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String s1 = "Allan Alda";
+        String s2 = "John wayne";
+        String s3 = "Gregory Peck";
+
+        System.out.println(s1 + ">>>lan A");
+        System.out.println(s2 + ">>>hn Wa");
+        System.out.println(s3 + ">>>egory P");
+
+
+
+
+
+
+
+    }
+}
